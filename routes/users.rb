96f58e41874652a576dc61a46d209d6fe5e998c7
Sameet Sapra <sameetandpotatoes@gmail.com>
2016-10-10 12:23:58 -0500

    Add S3 integration to users.rb

--- routes/users.rb
+++ routes/users.rb
@@ -1,5 +1,8 @@
 # encoding: UTF-8
 require_relative '../models/user'
+require 'aws/s3'
+require 'base64'
+require 'tempfile'
 
 get '/' do
     "This is the groot users service"
@@ -17,7 +20,7 @@ end
 
 post '/users' do
     string = request.body.read.gsub(/=>/, ":")
-    payload = JSON.parse (string || '{"name":"Not Given"}')
+    payload = JSON.parse(string)
     
     return [400, "Missing firstName"] unless payload["firstName"]
     return [400, "Missing netid"] unless payload["netid"]
@@ -33,17 +36,29 @@ post '/users' do
                 first_name: payload["firstName"],
                 last_name: payload["lastName"],
                 netid: payload["netid"],
+                status: "Pending",
                 date_joined: Time.now.getutc
             }
         ))
-        puts user.inspect
+        
+        # Upload to S3
+        AWS::S3::Base.establish_connection!(
+          access_key_id: 'AKIAJLP6MFOLVUSETVWA',
+          secret_access_key: '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'
+        )
+        
+        temp_file = Tempfile.new('foo')
+        File.open(temp_file, 'wb') {|f| f.write(Base64.decode64(payload["resume"]))}
+        puts temp_file.path
+        AWS::S3::S3Object.store(payload["netid"], File.read(temp_file.path), 'groot-recruiters-service-fs/resumes1')
     end
-    status = valid ? 201 : 403
+    status = valid ? 200 : 403
     return [status,format_response(user, request.accept)]
 end
 
 put '/users/:netid' do
     payload = JSON.parse(request.body.read)
+    
     return [400, "Missing first_name"] unless payload["first_name"]
     return [400, "Missing netid"] unless payload["netid"]
     return [400, "Missing last_name"] unless payload["last_name"]
@@ -52,7 +67,9 @@ put '/users/:netid' do
         first_name: payload["first_name"],
         last_name: payload["last_name"],
         netid: payload["netid"],
+        status: payload["status"]
     )
+    # TODO delete if payload["status"] == "Denied"
     return [status,format_response(quote, request.accept)]
 end
 
